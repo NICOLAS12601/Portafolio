894d60c7baaa189628870a7a8561b9ce0033c048
Nicolas del Campo <nicolas.delcampo@correo.ucu.edu.uym>
2025-03-29 10:21:56 -0300

    ejercicio4

---
+++ UT1/TA1/EJERCICIO4/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int aNumber=3;
+        if (aNumber >= 0)
+            if (aNumber == 0)
+                System.out.println("first string");
+            else System.out.println("second string");
+        System.out.println("third string");
+
+
+        System.out.println("Hello, World!");
+    }
+}
