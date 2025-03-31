089d4920dbde626c8cc2fd67310114a0eb275eea
Nicolas del Campo <nicolas.delcampo@correo.ucu.edu.uym>
2025-03-31 17:27:38 -0300

    Ejercicio2

---
+++ UT1/TA3/EJERCICIO2/src/main/java/org/example/ContadorDePalabras.java
@@ -0,0 +1,64 @@
+package org.example;
+
+public class ContadorDePalabras {
+
+    /*RECORRER LA PALABRA LETRA POR LETRA PARA ESTO LLEVO UN VARIABLE
+    AUXILIAR QUE SE VA INCREMENTADO HASTA EL TAMAÑO DEL LARGO DE LA FRASE.
+    UNA BANDERA QUE DICE ES PALABRA, LA CUAL PARA QUE SE LEVANTE NECESITA ENCONTRAR
+    AL MENOS UNA LETRA O UN NUMERO EN ES MOMENTO SE INCREMENTA EL CONTADOR, SOLO
+    CUANDA SE LEVANTA LA BANDERA, UNA VEZ LEVANTADASOLO SE BAJA CUANDO
+    ENCUENTRA UN ESPACIO.
+     */
+
+    public static int contadorPalabras(String frase) {
+        int resultado= 0;
+        boolean esPalabra =false;
+        int i=0;
+        do {
+            if(esPalabra==false){
+                if (Character.isLetter(frase.charAt(i))||Character.isDigit(frase.charAt(i))) {
+                    esPalabra = true;
+                    resultado++;}
+            }// queda levantada la bandera}
+            if (' ' == frase.charAt(i)) {
+                esPalabra = false;
+            }
+            i++;
+        }while(i<frase.length());
+
+        return resultado;}
+
+    /*Este metodo recorre el string letra caracter por caracter, si es una letra la compara
+    esta letra contra las letras almacenadas en array de char que tiene todas las vocales.
+    Si coincide con alguna aumenta la cantidad de vocales si no coincide aumenta las consonantes.
+     */
+
+    public static int[] contadorDeVocalesYConsonantes(String frase) {
+        int[] resultado= {0,0};// casilla 0 consonantes, casilla 1 vocales
+        int i=0;
+        char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+
+        do {
+            if (Character.isLetter(frase.charAt(i))) {
+                char letra = Character.toLowerCase(frase.charAt(i));
+                boolean esConsonante = true;
+                for (int j = 0; j < vocales.length; j++) {
+                    if (vocales[j] == letra) {
+                        resultado[1]++;//vocal
+                        esConsonante = false;
+                        break;
+                    }
+                }
+                if(esConsonante){
+                    resultado[0]++;//consonante
+                }
+            }
+            i++;
+        }while(i<frase.length());
+
+        return resultado;}
+
+}
+
+
+
---
+++ UT1/TA3/EJERCICIO2/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String frase = "HOLA COMO ESTAS";
+        System.out.println(ContadorDePalabras.contadorPalabras(frase));
+        int[] resultado =new int[2];
+        resultado=ContadorDePalabras.contadorDeVocalesYConsonantes(frase);
+        System.out.println("La cantidad de consonantes es: "+resultado[0]+" y la cantidad de vocales es: "+resultado[1]);
+    }
+}
